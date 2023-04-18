6529627211baea105894df72411424cdf64e030f
LighttCode <118662933+Pratham2403@users.noreply.github.com>
2023-04-18 05:54:39 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,29 @@
+/* =========== Preloader =========== */
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    preloader.style.display = "none";
+  }, 2000);
+});
+
+/* =========== Scroll Top =========== */
+const scrollTop = document.querySelector(".scroll-top");
+
+scrollTop.addEventListener("click", () => {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+window.addEventListener("scroll", (e) => {
+  const scrollHeight = window.pageYOffset;
+
+  if (scrollHeight > 300) {
+    scrollTop.classList.add("show");
+  } else {
+    scrollTop.classList.remove("show");
+  }
+});
